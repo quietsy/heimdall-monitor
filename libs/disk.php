9760475fc205d508088f193baefc1f06784f02e6
Quietsy <quietsy@gmail.com>
2020-10-05 22:47:31 +0300

    Update disk.php

--- libs/disk.php
+++ libs/disk.php
@@ -3,7 +3,7 @@ require '../autoload.php';
 
 $datas = array();
 
-if (!(exec('/bin/df -T | awk -v c=`/bin/df -T | grep -bo "Type" | awk -F: \'{print $2}\'` \'{print substr($0,c);}\' | tail -n +2 | sort -r -k 5 -i | awk \'{print $1","$2","$3","$4","$5","$6","$7}\'', $df)))
+if (!(exec('/bin/df -T -B1 | awk -v c=`/bin/df -T -B1 | grep -bo "Type" | awk -F: \'{print $2}\'` \'{print substr($0,c);}\' | tail -n +2 | sort -r -k 5 -i | awk \'{print $1","$2","$3","$4","$5","$6","$7}\'', $df)))
 {
     $datas[] = array(
         'total'         => 'N.A',
